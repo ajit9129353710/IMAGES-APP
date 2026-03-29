f6a8d5e6ab33341517de41ea4a9e0f2b48a5bd48
ajit9129353710 <prajapatiajit183@gmail.com>
2026-03-29 18:44:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import'./App.css';
+
+function App() {
+
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+
+  const ACCESS_KEY = "wAZPgixYkw5BESycUrYx0uXjF-HYjRFeSU5vGPlLQk8";
+
+  const searchImages = async () => {
+
+    const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setImages(data.results);
+  };
+
+  return (
+    <div className="container">
+
+      <h1>Image Search App</h1>
+
+<input
+  type="text"
+  placeholder="Search images..."
+  value={query}
+  onChange={(e) => setQuery(e.target.value)}
+  onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      searchImages();
+    }
+  }}
+/>
+
+      <button onClick={searchImages}>Search</button>
+
+      <div className="image-grid">
+        {images.map((img) => (
+          <img key={img.id} src={img.urls.small} alt="img"/>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
